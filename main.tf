e19e49c56547d8bd3c3171ea2c80f925e527294f
Mukund Deotale <mukundeotale9860@gmail.com>
2025-12-31 14:22:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "example" {
+  ami           = "ami-02b8269d5e85954ef"
+  instance_type = "t3.micro"
+  key_name = "project"
+  user_data = <<-EOF
+   #!/bin/bash
+   sudo -i
+   apt update -y
+   apt install apache2 -y
+   systemctl start apache2
+   echo "Hello Terraform" > /var/www/html/index.html
+  EOF
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
