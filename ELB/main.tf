befff23700dba5187476c378649d525e504c1c61
Mukund Deotale <mukundeotale9860@gmail.com>
2026-01-09 09:45:50 +0530

    Update main.tf

--- ELB/main.tf
+++ ELB/main.tf
@@ -40,10 +40,10 @@ resource "aws_launch_template" "example" {
   # Base64 encode the user_data
   user_data = base64encode(<<-EOF
               #!/bin/bash
-              yum update -y
-              yum install -y httpd
-              systemctl start httpd
-              systemctl enable httpd
+              apt update -y
+              apt install -y apache2
+              systemctl start apache2
+              systemctl enable apache2
               mkdir -p /var/www/html/mobile
               mkdir -p /var/www/html/laptop
 
@@ -69,8 +69,8 @@ resource "aws_launch_template" "example" {
               </html>
               EOF2
 
-              chown -R apache:apache /var/www/html
-              systemctl restart httpd
+              chown -R apache2:apache2 /var/www/html
+              systemctl restart apache2
 
               # Home page with instructions
               echo "<h1>HOME PAGE add /mobile-app /laptop" > /var/www/html/index.html
